28aa77cd65628264977845f0f2a00b0b38a7fb1d
Mateusz Gozdek <mateusz@kinvolk.io>
2020-11-04 23:33:11 +0100

    pkg/k8sutil: use go run and -mod=vendor for generating mocks
    So it is self contained within repository.
    
    Also silent lll linter on go:generate lines, as they might be long and
    it doesn't make sense to try to make them shorter.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- pkg/k8sutil/generate_mocks.go
+++ pkg/k8sutil/generate_mocks.go
@@ -1,3 +1,9 @@
 package k8sutil
 
-//go:generate mockgen -destination ./mocks/node_interface_mock.go k8s.io/client-go/kubernetes/typed/core/v1 NodeInterface
+//go:generate go run github.com/golang/mock/mockgen -build_flags=-mod=vendor -destination ./mocks/node_interface_mock.go k8s.io/client-go/kubernetes/typed/core/v1 NodeInterface
+
+import (
+	// Import model package to force it to be included in vendor/ directory,
+	// so go generate can run with -mod vendor.
+	_ "github.com/golang/mock/mockgen/model"
+)
