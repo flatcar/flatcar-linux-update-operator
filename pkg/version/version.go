66b271c953bdd869a2f8cc605c4cf16dc2235fd9
Mateusz Gozdek <mgozdek@microsoft.com>
2021-11-22 15:55:45 +0100

    pkg/version: rename v variable
    To avoid single letter variables dragging for too long as suggested by
    varnamelen linter.
    
    Signed-off-by: Mateusz Gozdek <mgozdek@microsoft.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -23,13 +23,13 @@ var Semver semver.Version
 
 //nolint:gochecknoinits
 func init() {
-	v, err := semver.Parse(Version)
+	parsedSemVer, err := semver.Parse(Version)
 	if err != nil {
 		klog.Fatalf("invalid build of update operator; version.Version must be set at compile "+
 			"time to a valid semver value. %v could not parse: %v", Version, err)
 	}
 
-	Semver = v
+	Semver = parsedSemVer
 }
 
 // Format formats Version and Commit variables into single string.
