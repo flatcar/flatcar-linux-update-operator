f1a2d525b2b41e5ce9ec33d99f25adac1529d697
Mateusz Gozdek <mgozdek@microsoft.com>
2022-04-05 11:51:00 +0200

    pkg/k8sutil: use fake client instead of gomock for testing
    This makes this tests more aligned with rest of the codebase and
    eliminates the need of using gomock completely, so we can drop code
    generation from the codebase which adds unnecessary complexity.
    
    Also using fake client is more type safe and natural as oppose to
    gomock.
    
    This commit also adds handling for more cases for function updating the
    node by following property based testing.
    
    Signed-off-by: Mateusz Gozdek <mgozdek@microsoft.com>

--- pkg/k8sutil/metadata_test.go
+++ pkg/k8sutil/metadata_test.go
@@ -6,74 +6,119 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/kubernetes/fake"
+	k8stesting "k8s.io/client-go/testing"
 
 	"github.com/flatcar-linux/flatcar-linux-update-operator/pkg/k8sutil"
-	mock_v1 "github.com/flatcar-linux/flatcar-linux-update-operator/pkg/k8sutil/mocks"
 )
 
-func atomicCounterIncrement(t *testing.T) func(n *corev1.Node) {
-	t.Helper()
-
-	return func(n *corev1.Node) {
-		counterAnno := "counter"
-		s := n.Annotations[counterAnno]
-
-		var i int
-
-		if s == "" {
-			i = 0
-		} else {
-			var err error
-			i, err = strconv.Atoi(s)
-			if err != nil {
-				t.Fatalf("parsing %q to integer: %v", s, err)
-			}
-		}
-
-		n.Annotations[counterAnno] = strconv.Itoa(i + 1)
-	}
-}
-
-func TestUpdateNodeRetryHandlesConflict(t *testing.T) {
+//nolint:funlen // Just subtests.
+func Test_Updating_node(t *testing.T) {
 	t.Parallel()
 
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	mockNi := mock_v1.NewMockNodeInterface(ctrl)
-	mockNode := &corev1.Node{}
-	mockNode.SetName("mock_node")
-	mockNode.SetNamespace("default")
-	mockNode.SetAnnotations(map[string]string{"counter": "20"})
-	mockNode.SetResourceVersion("20")
+	t.Run("retries_on_conflict_error", func(t *testing.T) {
+		t.Parallel()
 
-	mockNi.EXPECT().Get(context.TODO(), "mock_node", metav1.GetOptions{}).Return(mockNode, nil).AnyTimes()
+		annotationKey := "counter"
 
-	// Conflict once; mock that a third party incremented the counter from '20'
-	// to '21' right after the node is returned
-	gomock.InOrder(
-		mockNi.EXPECT().Update(context.TODO(), mockNode, metav1.UpdateOptions{}).Do(func(
-			ctx context.Context, n *corev1.Node, uo metav1.UpdateOptions) {
-			// Fake conflict; the counter was incremented elsewhere; resourceVersion is now 21
-			mockNode.SetAnnotations(map[string]string{"counter": "21"})
-			mockNode.SetResourceVersion("21")
-		},
-		).Return(mockNode, errors.NewConflict(schema.GroupResource{}, "mock_node", fmt.Errorf("err"))),
+		node := &corev1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "testNodeName",
+				Annotations: map[string]string{annotationKey: "20"},
+			},
+		}
 
-		// And then the successful retry
-		mockNi.EXPECT().Update(context.TODO(), mockNode, metav1.UpdateOptions{}).Return(mockNode, nil),
-	)
+		fakeClient := fake.NewSimpleClientset(node)
 
-	err := k8sutil.UpdateNodeRetry(context.TODO(), mockNi, "mock_node", atomicCounterIncrement(t))
-	if err != nil {
-		t.Errorf("unexpected error: expected increment to succeed")
-	}
+		fakeClient.PrependReactor("get", "nodes", func(action k8stesting.Action) (bool, runtime.Object, error) {
+			return true, node, nil
+		})
 
-	if mockNode.Annotations["counter"] != "22" {
-		t.Errorf("expected the counter to hit 22; was %v", mockNode.Annotations["counter"])
+		sentConflict := false
+
+		fakeClient.PrependReactor("update", "nodes", func(action k8stesting.Action) (bool, runtime.Object, error) {
+			if sentConflict {
+				return false, nil, nil
+			}
+
+			sentConflict = true
+
+			node.Annotations[annotationKey] = "21"
+
+			return true, node, errors.NewConflict(schema.GroupResource{}, node.Name, fmt.Errorf("test error"))
+		})
+
+		ctx := context.TODO()
+		nc := fakeClient.CoreV1().Nodes()
+
+		if err := k8sutil.UpdateNodeRetry(ctx, nc, node.Name, atomicCounterIncrement(t, annotationKey)); err != nil {
+			t.Fatalf("Unexpected error updating node: %v", err)
+		}
+
+		expectedCounterValue := "22"
+
+		if v := node.Annotations[annotationKey]; v != expectedCounterValue {
+			t.Fatalf("Expected the counter to hit %q, got %q", expectedCounterValue, v)
+		}
+	})
+
+	t.Run("returns_error_when", func(t *testing.T) {
+		t.Parallel()
+
+		t.Run("updated_node_does_not_exist", func(t *testing.T) {
+			t.Parallel()
+
+			fakeClient := fake.NewSimpleClientset()
+
+			ctx := context.TODO()
+			nc := fakeClient.CoreV1().Nodes()
+
+			if err := k8sutil.UpdateNodeRetry(ctx, nc, "nonExistingNode", func(*corev1.Node) {}); err == nil {
+				t.Fatalf("Expected error updating non existing node")
+			}
+		})
+
+		t.Run("updating_node_returns_error_other_than_conflict", func(t *testing.T) {
+			t.Parallel()
+
+			node := &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "testNodeName",
+				},
+			}
+
+			fakeClient := fake.NewSimpleClientset(node)
+
+			fakeClient.PrependReactor("update", "nodes", func(action k8stesting.Action) (bool, runtime.Object, error) {
+				return true, nil, fmt.Errorf("test error")
+			})
+
+			ctx := context.TODO()
+			nc := fakeClient.CoreV1().Nodes()
+
+			if err := k8sutil.UpdateNodeRetry(ctx, nc, node.Name, func(*corev1.Node) {}); err == nil {
+				t.Fatalf("Expected error updating node")
+			}
+		})
+	})
+}
+
+func atomicCounterIncrement(t *testing.T, annotationKey string) func(n *corev1.Node) {
+	t.Helper()
+
+	return func(node *corev1.Node) {
+		s := node.Annotations[annotationKey]
+
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			t.Fatalf("Parsing %q to integer: %v", s, err)
+		}
+
+		node.Annotations[annotationKey] = strconv.Itoa(i + 1)
 	}
 }
